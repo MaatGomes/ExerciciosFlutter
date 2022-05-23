f864dec4a7390bf94d66a920c10953f709746982
Mateus Gomes Veloso - MGE <mateus.gomes@iteris.com.br>
2022-05-23 15:31:41 -0300

    Exercício 1 do PDF 01-flutter

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text('Página Inicial'),
+      ),
+      body: Column(children: const [
+        Center(
+          heightFactor: 19,
+          child: Text('Olá Mundo!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.white,
+              )),
+        ),
+      ]),
+      backgroundColor: Colors.blue[900],
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Exemplo de botão',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    );
+  }
+}
